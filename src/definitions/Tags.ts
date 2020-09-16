4eeca94a918a31f1ad051e8d5e4c85ad6a8b8e97
Gijs Burghoorn <g.burghoorn@gmail.com>
2020-09-16 12:36:57 +0200

    Definitions added

---
+++ src/definitions/Tags.ts
@@ -0,0 +1,368 @@
+enum Tags {
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/a)
+     */
+    A = 'a',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/animate)
+     */
+    Animate = 'animate',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/animateMotion)
+     */
+    AnimateMotion = 'animateMotion',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/animateTransform)
+     */
+    AnimateTransform = 'animateTransform',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/circle)
+     */
+    Circle = 'circle',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/clipPath)
+     */
+    ClipPath = 'clipPath',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/color-profile)
+     */
+    ColorProfile = 'color-profile',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/defs)
+     */
+    Defs = 'defs',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/desc)
+     */
+    Desc = 'desc',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/discard)
+     */
+    Discard = 'discard',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/ellipse)
+     */
+    Ellipse = 'ellipse',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feBlend)
+     */
+    FeBlend = 'feBlend',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feColorMatrix)
+     */
+    FeColorMatrix = 'feColorMatrix',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feComponentTransfer)
+     */
+    FeComponentTransfer = 'feComponentTransfer',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feComposite)
+     */
+    FeComposite = 'feComposite',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feConvolveMatrix)
+     */
+    FeConvolveMatrix = 'feConvolveMatrix',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feDiffuseLighting)
+     */
+    FeDiffuseLighting = 'feDiffuseLighting',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feDisplacementMap)
+     */
+    FeDisplacementMap = 'feDisplacementMap',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feDistantLight)
+     */
+    FeDistantLight = 'feDistantLight',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feDropShadow)
+     */
+    FeDropShadow = 'feDropShadow',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feFlood)
+     */
+    FeFlood = 'feFlood',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feFuncA)
+     */
+    FeFuncA = 'feFuncA',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feFuncB)
+     */
+    FeFuncB = 'feFuncB',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feFuncG)
+     */
+    FeFuncG = 'feFuncG',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feFuncR)
+     */
+    FeFuncR = 'feFuncR',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feGaussianBlur)
+     */
+    FeGaussianBlur = 'feGaussianBlur',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feImage)
+     */
+    FeImage = 'feImage',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feMerge)
+     */
+    FeMerge = 'feMerge',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feMergeNode)
+     */
+    FeMergeNode = 'feMergeNode',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feMorphology)
+     */
+    FeMorphology = 'feMorphology',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feOffset)
+     */
+    FeOffset = 'feOffset',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/fePointLight)
+     */
+    FePointLight = 'fePointLight',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feSpecularLighting)
+     */
+    FeSpecularLighting = 'feSpecularLighting',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feSpotLight)
+     */
+    FeSpotLight = 'feSpotLight',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feTile)
+     */
+    FeTile = 'feTile',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feTurbulence)
+     */
+    FeTurbulence = 'feTurbulence',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/filter)
+     */
+    Filter = 'filter',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/foreignObject)
+     */
+    ForeignObject = 'foreignObject',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/g)
+     */
+    G = 'g',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/hatch)
+     */
+    Hatch = 'hatch',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/hatchpath)
+     */
+    Hatchpath = 'hatchpath',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/image)
+     */
+    Image = 'image',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/line)
+     */
+    Line = 'line',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/linearGradient)
+     */
+    LinearGradient = 'linearGradient',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/marker)
+     */
+    Marker = 'marker',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/mask)
+     */
+    Mask = 'mask',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element)
+     */
+    Mesh = 'mesh',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element)
+     */
+    Meshgradient = 'meshgradient',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element)
+     */
+    Meshpatch = 'meshpatch',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element)
+     */
+    Meshrow = 'meshrow',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/metadata)
+     */
+    Metadata = 'metadata',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/mpath)
+     */
+    Mpath = 'mpath',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/path)
+     */
+    Path = 'path',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/pattern)
+     */
+    Pattern = 'pattern',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/polygon)
+     */
+    Polygon = 'polygon',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/polyline)
+     */
+    Polyline = 'polyline',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/radialGradient)
+     */
+    RadialGradient = 'radialGradient',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/rect)
+     */
+    Rect = 'rect',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/script)
+     */
+    Script = 'script',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/set)
+     */
+    Set = 'set',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/solidcolor)
+     */
+    Solidcolor = 'solidcolor',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/stop)
+     */
+    Stop = 'stop',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/style)
+     */
+    Style = 'style',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/svg)
+     */
+    Svg = 'svg',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/switch)
+     */
+    Switch = 'switch',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/symbol)
+     */
+    Symbol = 'symbol',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/text)
+     */
+    Text = 'text',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/textPath)
+     */
+    TextPath = 'textPath',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/title)
+     */
+    Title = 'title',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/tspan)
+     */
+    Tspan = 'tspan',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element)
+     */
+    Unknown = 'unknown',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/use)
+     */
+    Use = 'use',
+
+    /**
+     * [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/SVG/Element/view)
+     */
+    View = 'view',
+}
+
+export default Tags
---
+++ src/definitions/index.ts
@@ -0,0 +1,5 @@
+import Events from './Events'
+import Tags from './Tags'
+import Attributes from './Attributes'
+
+export { Events as SVGEvents, Tags as SVGTags, Attributes as SVGAttrs }
