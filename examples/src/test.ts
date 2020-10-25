6ad8646e96df757227315c916a710460ead4adc1
Gijs Burghoorn <g.burghoorn@gmail.com>
2020-10-25 15:57:44 +0100

    Using local dependency

--- examples/src/test.ts
+++ examples/src/test.ts
@@ -18,10 +18,23 @@
 
 // manager.render(svgCircle(10, new V2D(40, 40)).fill('lightblue'))
 
-import { SVGManager } from '../../dist'
-import { SVGViewBox, V2D } from '../../dist/helpers'
-import { svgCircle } from '../../dist/shapes'
-import StaticSizeComponent from '../../dist/components/StaticSize'
+import {
+    bgPattern,
+    hightlighting,
+    moving,
+    panning,
+    PolygonComponent,
+    resizing,
+    SizedComponent,
+    StaticComponent,
+    svgCircle,
+    svgEllipse,
+    svgLines,
+    SVGManager,
+    SVGViewBox,
+    V2D,
+    zooming,
+} from 'ts-svgmanager'
 
 // Initialize the SVGManager
 const manager = new SVGManager()
@@ -29,13 +42,38 @@ const manager = new SVGManager()
     .set('viewBox', new SVGViewBox(0, 0, 100, 50))
     .styleSet('border', '1px solid #000')
 
-manager.render(svgCircle(10, new V2D(50, 25))).fill('coral')
+manager.utilize(panning(), bgPattern({ patternType: 'dotted' }), zooming())
 
-const staticSized = new StaticSizeComponent('static', () =>
-    svgCircle(10).fill('coral'),
+const staticComponent = new StaticComponent(
+    'static',
+    svgCircle(10).fill('lightblue'),
 )
+staticComponent.utilize(moving(), hightlighting())
 
-manager.declare(staticSized)
+const variableComponent = new SizedComponent('variable', (size: V2D) =>
+    svgEllipse(new V2D(size.x / 2, size.y / 2)).fill('coral'),
+)
+variableComponent.utilize(moving(), resizing())
 
-const instance = manager.instantiate('static')
-manager.move(instance, new V2D(50, 25))
+const pointsComponent = new PolygonComponent('points', (points: V2D[]) =>
+    svgLines(points),
+)
+pointsComponent.utilize(moving(), resizing())
+
+manager.declare(staticComponent)
+manager.declare(variableComponent)
+manager.declare(pointsComponent)
+
+manager.create('points', new V2D(15, 15), [
+    new V2D(0, 0),
+    new V2D(10, 10),
+    new V2D(20, 0),
+])
+
+manager.create('variable', new V2D(15, 15), [new V2D(10, 10)])
+
+manager.create('static', new V2D(20, 30))
+manager.create('static', new V2D(60, 30))
+
+// const instance = manager.instantiate('static')
+// manager.move(instance, new V2D(50, 25))
